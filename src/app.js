db189a66798061105c2ec7289cec65df376a4847
thanhtan190 <thanhtan190@gmail.com>
2018-04-12 00:07:04 +0900

    split action and reducer, add middleware and logger when store, end build redux

--- src/app.js
+++ src/app.js
@@ -1,50 +1,53 @@
 "use strict"
 
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+
+import logger from 'redux-logger';
 
 import reducers from './reducers/index';
 
-const store = createStore(reducers)
+import {postBooks, deleteBooks, updateBooks} from './actions/booksActions';
+
+import { addToCart } from "./actions/cartActions";
+
+const middleware = applyMiddleware(logger);
+
+const store = createStore(reducers, middleware);
 
 store.subscribe(function(){
     console.log('current state is:', store.getState());
 })
 
-store.dispatch({
-    type: "POST_BOOK",
-    payload: [{
-        id: 1,
-        title: 'book1',
-        description: 'this is the book description',
-        price: 30
-    },{
-        id: 2,
-        title: 'book2',
-        description: 'this is the book description',
-        price: 50
-    }]
-})
+store.dispatch(
+    postBooks(
+        [{
+            id: 1,
+            title: 'book1',
+            description: 'this is the book description',
+            price: 30
+        },{
+            id: 2,
+            title: 'book2',
+            description: 'this is the book description',
+            price: 50
+        }]
+    )   
+)
 
-store.dispatch({
-    type: "DELETE_BOOK",
-    payload: {
-        id : 1
-    }
-})
+store.dispatch(
+    deleteBooks({id:1})
+)
 
-store.dispatch({
-    type: "UPDATE_BOOK",
-    payload: {
-        id : 2,
-        price: 100,
-        title: 'I updated my book'
-    }
-})
+store.dispatch(
+    updateBooks({
+        id :2,
+        title: 'updated book'
+    })
+)
 
-store.dispatch({
-    type: "ADD_TO_CART",
-    payload: [{
+store.dispatch(
+    addToCart([{
         id : 2,
         price: 110,
-    }]
-})
+    }])
+)
